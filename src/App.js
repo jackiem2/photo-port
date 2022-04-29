c4ecf1923075c04cca3b35905e82bad79a77e959
jackiem2 <jacqueline.merlan@gmail.com>
2022-04-28 22:08:21 -0500

    created nav section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
-import React from 'react';
+//import React from 'react';
 import About from './components/About';
+import Nav from './components/Nav';
 
 function App() {
 
   return (
     <div>
+        <Nav></Nav>
       <main>
         <About></About>
       </main>
